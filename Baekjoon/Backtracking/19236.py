aad08e2cf126f59c64ca3bf3d333e78cc9059ce0
Ethan-lsh <ynd52156@gmail.com>
2024-04-01 17:52:34 +0900

    ADD:Backtracking

--- Baekjoon/Backtracking/19236.py
+++ Baekjoon/Backtracking/19236.py
@@ -3,6 +3,20 @@
 # Description: Practice for backtracking of SWEA
 # Code refer: https://developer-ellen.tistory.com/68
 
+"""
+Condition.
+1. 1x1 격자에 있는 물고기는 사이즈, 방향 데이터를 갖는다.
+2. 격자에 있는 모든 물고기의 이동이 종료되면, 상어는 현재 방향에 따라 이동하며 먹을 수 있는 물고기를 먹는다.
+3. 상어가 물고기를 먹으면, 사이즈가 1 증가한다.
+
+Solution.
+1. 먼저, 물고기를 이동시킨 다음 상어를 움직인다.
+2. 물고기 번호가 작은 순서부터 이동할 수 있도록, for 반복문을 1~17까지 범위를 지정해 현재 번호에 맞는 물고기를 격자로 부터 찾는다.
+2-1) 물고기를 찾으면 방향이 가리키는 값과 위치를 바꾼다.
+3. 모든 물고기의 이동이 끝나면 상어를 이동시킨다.
+"""
+
+
 import copy
 from collections import deque
 
